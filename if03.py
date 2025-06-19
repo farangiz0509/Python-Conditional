19a8b8b8b6078c4130457bfdbf07c14afbb82cee
farangiz0509 <farangiztolibova75@gmail.com>
2025-06-19 19:10:49 +0545

    done

--- if03.py
+++ if03.py
@@ -1,4 +1,12 @@
 number = int(input("enter number"))
 
+print("chiqish:")
 
-if number
+if number % 2 == 0:
+    print(f'{number} soni 2 bolganda qoldiqsiz boinadi')
+    
+if number % 3 == 0:
+    print(f'{number}soni 3 bolganda qoldiqsiz boinadi')
+
+if number % 5 == 0:
+    print(f'{number}soni 5 bolganda qoldiqsiz boinadi')
