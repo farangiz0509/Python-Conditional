2a5e230c7a17e5eda6c5357eea07a7bba95dc8eb
farangiz0509 <farangiztolibova75@gmail.com>
2025-06-19 21:54:34 +0545

    done

--- if05.py
+++ if05.py
@@ -1,1 +1,13 @@
+tempurature = int(input("enter tempurature"))
+a = " very cold wear warm clothes"
+b = "cold wear coat"
+c = "the weather is good"
 
+
+if tempurature < 0:
+    print(a)
+if tempurature >= 0 and tempurature <= 17:
+    print(b)
+if tempurature > 15:
+    print(c)
+    
