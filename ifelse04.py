2ae2380dbfc7dde9381890c9d5012976cbf3ac07
farangiz0509 <farangiztolibova75@gmail.com>
2025-06-19 23:06:43 +0545

    done

--- ifelse04.py
+++ ifelse04.py
@@ -1,1 +1,9 @@
+balance = 5000.0
 
+input = float(input("how much do you want to get"))
+
+if input >= 0 :
+    if input <= balance :
+        print(f"money was taken. left :{balance} som")
+    else :
+        print("money is not enough. your balance {balance}som")
--- ifelse05.py
+++ ifelse05.py
@@ -1,1 +1,6 @@
+email = input("enter your email:")
 
+if "@" in email and (".com" in email or ".uz" in email or ".org" in email):
+    print(" email is in correct form")
+else:
+    print(" email is in correct form")
