6f8fd57ea0593deb9402befc368508cfc728e49a
farangiz0509 <farangiztolibova75@gmail.com>
2025-06-19 22:18:57 +0545

    done

--- ifelse02.py
+++ ifelse02.py
@@ -1,1 +1,10 @@
+password = input("enter password:")
 
+number = ("0" in password or "1" )
+letter = ("A" in password or "a")
+
+if len(password) >= 8 and number and letter:
+    
+    print("password is true")
+else:
+    print(" your password is incorrect it should include 1 number and 1 letter")
