438e6a2e34f73c8a4e16bd183702e37d9c0dc32f
farangiz0509 <farangiztolibova75@gmail.com>
2025-06-19 22:43:41 +0545

    done

--- ifelse03.py
+++ ifelse03.py
@@ -1,1 +1,7 @@
+import os 
 
+name_of_file = input("enter the name:")
+if os.path.exists(name_of_file):
+    print(f"there is {name_of_file} file")
+else:
+    print(f"there is not {name_of_file}file")
